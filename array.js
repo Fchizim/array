9ee5efefda1440ced51bbfa76667e296635628bd
Fred <104666842+Fchizim@users.noreply.github.com>
2023-10-23 22:01:15 +0100

    Add files via upload

---
+++ array.js
@@ -0,0 +1,5 @@
+const Name = [`chinonso`, `collin`, `Cosmos`, `Treasure`, `Fred`]
+const DOB = [2016, 2017, 2018, 2015, 2018]
+Name.pop(prompt())
+
+document.getElementById("demo").innerHTML= `${Name}   ${DOB}`   
